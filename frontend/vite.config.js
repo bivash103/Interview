44b98a4b693f6aa615977e9378c994856386d21c
bivash103 <bivashp500@gmail.com>
2026-06-30 12:29:47 +0530

    Remove Tailwind Typography plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  // if using React
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-
-  theme: {
-    extend: {},
-  },
   plugins: [
     react(),
     tailwindcss(),
-    require("@tailwindcss/typography"),
   ],
-})
+});
